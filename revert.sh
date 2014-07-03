02e4951e386abab03600f6510c1c446124bf81d0
Thilo Fischer <thilo-fischer@gmx.de>
2014-07-03 06:09:11 +0000

    scripts to automate copying the files

--- revert.sh
+++ revert.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
 
-cat asus-based/asusdec.kl  > /system/usr/keylayout/asusdec.kl
+# Write key layout and character map files for a specific keyboard device
+# from the current directory to the appropriate locations.
+#
+# !! Make sure to backup the original versions of the affected files BEFORE
+# !! running this script for the first time! E.g.
+# !! # cp -i /system/usr/keylayout/asusdec.kl /system/usr/keychars/asusdec.kcm backup-directory/
+#
+# This file is configured to write the original files for the Asus Transformer
+# Pad TF300T Mobile Docking keyboard back to their original location.
+# Just adapt DEVICE_NAME and SRC_SUFFIX to make it work for different files.
+#
+# Instead of copying the files, new content is written into the existing files
+# to keep the original file permissions.
+#
+# Run this file as root when in the directory where the designated versions 
+# of asusdec.k{l,cm} reside. Run as a bash script or (in case it resides on a
+# noexec file system) just source it.
+#
+# You will need write permissions to /system to run the script:
+#  # mount -o remount,rw /system
+# Use busybox's mount command in case the Android built-in mount command
+# does not support the remount option.
+
+# for Asus Transformer Pad TF300T mobile docking
+DEVICE_NAME="asusdec"
+# suffix (appended to DEVICE_NAME) of those files to be copied
+SRC_SUFFIX="_original"
+
+cat $DEVICE_NAME$SRC_SUFFIX.kl  > /system/usr/keylayout/$DEVICE_NAME.kl
 SUCCESS=$?
 
-cat asus-based/asusdec.kcm > /system/usr/keychars/asusdec.kcm
+cat $DEVICE_NAME$SRC_SUFFIX.kcm > /system/usr/keychars/$DEVICE_NAME.kcm
 SUCCESS=$SUCCESS$?
 
 test x$SUCCESS = x00
--- rollout.sh
+++ rollout.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 
-# Write key layout and character map files to the appropriate locations.
+# Write key layout and character map files for a specific keyboard device
+# from the current directory to the appropriate locations.
 #
-# Instead of copying the files, write new content into the existing files
+# !! Make sure to backup the original versions of the affected files BEFORE
+# !! running this script for the first time! E.g.
+# !! # cp -i /system/usr/keylayout/asusdec.kl /system/usr/keychars/asusdec.kcm backup-directory/
+#
+# This file is configured to write files for the Asus Transformer Pad TF300T
+# Mobile Docking keyboard modified to support the Neo2 keyboard layout.
+# Just adapt DEVICE_NAME and SRC_SUFFIX to make it work for different files.
+#
+# Instead of copying the files, new content is written into the existing files
 # to keep the original file permissions.
+#
 # Run this file as root when in the directory where the designated versions 
 # of asusdec.k{l,cm} reside. Run as a bash script or (in case it resides on a
 # noexec file system) just source it.
-#
-# Make sure to backup the original versions of the affected files BEFORE
-# running this script for the first time!
-#  # cp -i /system/usr/keylayout/asusdec.kl /system/usr/keychars/asusdec.kcm backup-directory/
 #
 # You will need write permissions to /system to run the script:
 #  # mount -o remount,rw /system
 # Use busybox's mount command in case the Android built-in mount command
 # does not support the remount option.
 
-cat asusdec.kl  > /system/usr/keylayout/asusdec.kl
+# for Asus Transformer Pad TF300T mobile docking
+DEVICE_NAME="asusdec"
+# suffix (appended to DEVICE_NAME) of those files to be copied
+SRC_SUFFIX="_neo2"
+
+cat $DEVICE_NAME$SRC_SUFFIX.kl  > /system/usr/keylayout/$DEVICE_NAME.kl
 SUCCESS=$?
 
-cat asusdec.kcm > /system/usr/keychars/asusdec.kcm
+cat $DEVICE_NAME$SRC_SUFFIX.kcm > /system/usr/keychars/$DEVICE_NAME.kcm
 SUCCESS=$SUCCESS$?
 
 test x$SUCCESS = x00
